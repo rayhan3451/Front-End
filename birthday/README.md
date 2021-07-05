edd4ed061ccde0c870fec2f20a502c75fcf972e5
rayhan <md.rayhan3451@gmail.com>
2021-07-05 12:47:44 +0600

    birthday added

---
+++ birthday/README.md
@@ -0,0 +1,3 @@
+# Used Hooks
+
+- useState
---
+++ birthday/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Tayeeba Zafrin',
+    age: 22,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Tabanoom Mim',
+    age: 21,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Hasanuzzaman Rafin',
+    age: 22,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Mohammad Rayhan',
+    age: 24,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Roushan Jahan Upoma',
+    age: 14,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
